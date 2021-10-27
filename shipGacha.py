0679aacefbb6b887521a8e86be500deb5f5c3190
ShadowAlzazel <92663497+ShadowAlzazel@users.noreply.github.com>
2021-10-27 15:35:26 -0700

    Add files via upload

--- shipGacha.py
+++ shipGacha.py
@@ -24,14 +24,19 @@ with open("currentShipClasses.json", "r") as currentShipClassesFile:
 def getClass(strShipClass):
     return getattr(sys.modules[__name__], strShipClass)
 
-def ranShipName():
-    x = randint(0, len(randomShipNames) - 1)
+
+#Get random name from json file
+def ranShipName(r = True, n = 0):
+    if r == True:
+        x = randint(0, len(randomShipNames) - 1)
+    else:
+        x = n
     y = randint(0, len(randomShipNames[x]['names']) - 1)
 
     rName = randomShipNames[x]['names'][y]
     return rName
 
-
+#Get random Class from json file
 def ranShipClass():
     x = randint(0, len(currentShipClasses) - 1)
     y = randint(0, len(currentShipClasses[x]['currentClasses']) - 1)
@@ -39,13 +44,22 @@ def ranShipClass():
     rClass = currentShipClasses[x]['currentClasses'][y]
     return rClass
 
-
+#create a ship with random stats
 def createGachaShip():
-    n = randint(1, 10000)
+    h = randint(1, 10000)
+    l = len(shipLib2)
+
     gachaClass = getClass(ranShipClass())
-    gachaShip = gachaClass(n, ranShipName())
+    gachaShip = gachaClass(h, ranShipName())
     shipLib2.append(gachaShip)
+
+    shipLib2[l].shipStats['FP'] += (randint(-10, 15))
+    shipLib2[l].shipStats['ACC'] += (randint(-2, 3))
+    shipLib2[l].shipStats['EVA'] += (randint(-3, 4))
+    shipLib2[l].shipStats['SPD'] += (randint(-2, 2))
+
 
 #print(ranShipName())
 #print(ranShipClass())
 createGachaShip()
+print(shipLib2[0].shipStats)
