07212dce0b318515c743aebaa3ee18b74984b7bd
ShadowAlzazel <92663497+ShadowAlzazel@users.noreply.github.com>
2021-11-06 17:54:00 -0700

    Create __init__.py

---
+++ shipClasses/__init__.py
@@ -0,0 +1,9 @@
+print("shipClasses Module")
+from shipClasses.typeBB import *
+from shipClasses.typeBC import *
+from shipClasses.typeCS import *
+from shipClasses.typeCA import *
+from shipClasses.typeCL import *
+from shipClasses.typeDD import *
+
+#from . classesCL import *
