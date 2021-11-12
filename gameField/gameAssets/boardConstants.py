35567167878e234e22e32ab1d705f012905543fd
ShadowAlzazel <pastenalfredojr@gmail.com>
2021-11-12 00:39:36 -0800

    add seperate image folder

--- gameField/gameAssets/boardConstants.py
+++ gameField/gameAssets/boardConstants.py
@@ -1,5 +1,4 @@
 #game constants
-import pygame
 
 LENGTH = 1000
 WIDTH = 750
@@ -9,10 +8,3 @@ CENTER_Y = WIDTH // 2
 HEX_SIZE = 64
 
 FPS = 60
-
-SCREEN_RGB = [38, 38, 51]
-
-
-GAME_ICON = pygame.image.load('gameField/gameAssets/shipIconP2.png')
-HEX_Y_IMG = pygame.image.load('gameField/gameAssets/emptyHex.png')
-HEX_ENTF_IMG = pygame.image.load('gameField/gameAssets/shipHereHex.png')
---
+++ gameField/gameAssets/boardImages.py
@@ -0,0 +1,9 @@
+#images
+import pygame
+
+SCREEN_RGB = [38, 38, 51]
+
+
+GAME_ICON = pygame.image.load('gameField/gameAssets/shipIconP2.png')
+HEX_Y_IMG = pygame.image.load('gameField/gameAssets/emptyHex.png')
+HEX_ENTF_IMG = pygame.image.load('gameField/gameAssets/shipHereHex.png')
