fdba859613d4031a5e092993d3062b9dcf0bb50e
ShadowAlzazel <pastenalfredojr@gmail.com>
2021-11-23 17:15:28 -0800

    file management

--- gameField/__init__.py
+++ gameField/__init__.py
@@ -1,3 +1,3 @@
-from gameField.gameBoard import *
-from gameField.gameSpace import *
+from gameField.gameWindow import *
+from gameField.spaceGame import *
 from gameField.gameFont import Font
--- starSpaces/__init__.py
+++ starSpaces/__init__.py
@@ -1,3 +1,3 @@
 #from spaceField.spaceHexes import *
 #from spaceField.spaceObjects import *
-from spaceField.combatSpace import *
+from starSpaces.combatSpace import *
