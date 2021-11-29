06b994da09576ffca3606cbf82358f897aaa395e
ShadowAlzazel <pastenalfredojr@gmail.com>
2021-11-29 00:01:00 -0800

    changed missiles

--- shipClasses/shipEquipment/shipWeapons/__init__.py
+++ shipClasses/shipEquipment/shipWeapons/__init__.py
@@ -2,7 +2,7 @@ from shipClasses.shipEquipment.shipWeapons.shipArcThrowers import *
 from shipClasses.shipEquipment.shipWeapons.shipIonLances import *
 from shipClasses.shipEquipment.shipWeapons.shipMassDrivers import *
 from shipClasses.shipEquipment.shipWeapons.shipPointDefense import *
-from shipClasses.shipEquipment.shipWeapons.shipMissiles import * 
+from shipClasses.shipEquipment.shipWeapons.shipMissileSystems import * 
 from shipClasses.shipEquipment.shipWeapons.shipPlasmaPhasers import *
 from shipClasses.shipEquipment.shipWeapons.shipTorpedoes import *
 from shipClasses.shipEquipment.shipWeapons.shipEnergyLasers import *
